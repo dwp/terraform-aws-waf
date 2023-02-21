4d48ee419872a95b0dcc25ea70eabb736ad50fd4
rbryett <13303701+rbryett@users.noreply.github.com>
2023-02-21 12:40:31 +0000

    DTWRKS-854 initial commit (#6)
    * DTWRKS-854 initial commit
    
    * DTWRKS-854 basic terratests for terraform-aws-waf
    
    * DTWRKS-854 pre-commit rules
    
    * DTWRKS-854
    
    * DTWRKS-854
    
    * DTWRKS-854
    
    * DTWRKS-854
    
    * DTWRKS-854
    
    * DTWRKS-854
    
    ---------
    
    Co-authored-by: rorybryett <rory.bryett@dwp.gov.uk>

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "wafregional_web_acl_id" {
-  value = aws_wafregional_web_acl.acl.id
+  value       = aws_wafregional_web_acl.acl.id
+  description = "The ID of the regional Web ACL."
 }
---
+++ tests/terratest/waf_test.go
@@ -0,0 +1,74 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+
+	"path/filepath"
+
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+// Terratest functions for testing the WAF module
+func TestWAF(t *testing.T) {
+	t.Parallel()
+
+	// AWS Region set as eu-west-1 as standard.
+	awsRegion := "eu-west-1"
+
+	// set up variables for other module variables so assertions may be made on them later
+
+	// Terraform plan.out File Path
+	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../..", "example/")
+	planFilePath := filepath.Join(exampleFolder, "plan.out")
+
+
+	terraformOptionsWAF := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../../example",
+		Upgrade:      true,
+
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+
+		},
+
+		//Environment variables to set when running Terraform
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+
+		// Configure a plan file path so we can introspect the plan and make assertions about it.
+		PlanFilePath: planFilePath,
+	})
+
+	// website::tag::2::Run `terraform init`, `terraform plan`, and `terraform show` and fail the test if there are any errors
+	plan := terraform.InitAndPlanAndShowWithStruct(t, terraformOptionsWAF)
+
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptionsWAF)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptionsWAF)
+
+	// website::tag::3::Use the go struct to introspect the plan values.
+	terraform.RequirePlannedValuesMapKeyExists(t, plan, "module.waf.aws_wafregional_web_acl.acl")
+
+
+	// Run `terraform output` to get the value of an output variable
+	// Retrieve all the outputs from the module
+	wafAclID := terraform.Output(t, terraformOptionsWAF, "wafregional_web_acl_id")
+
+
+	// Assert that all output Variables are populated
+	lengthOfWafACLID := len(wafAclID)
+
+
+	// Verify the Waf parameters
+	assert.NotEqual(t, lengthOfWafACLID, 0, "WAF ACL ID Output MUST be populated")
+
+
+}
